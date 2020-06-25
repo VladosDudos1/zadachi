a355d0bc27d9826ef429370ac9b9a65f37d103b0
VladosDudos1 <55552724+VladosDudos1@users.noreply.github.com>
2020-06-25 15:11:44 +0300

    sets

--- src/mnozhestva/2.kt
+++ src/mnozhestva/2.kt
@@ -1,5 +1,20 @@
 package mnozhestva
 
-fun main() {
+// даны 2 множества, определить типы общих элемнтов
 
+fun main() {
+    val set1 = setOf<Any>("50", 6, 0.73, true, arrayListOf(1, 2))
+    val set2 = setOf<Any>(6, true, arrayListOf(1, 2), 0.43, "Привет")
+
+    val list = set1.intersect(set2).toList()
+
+    for (i in list) {
+        val newI = i.javaClass.toString()
+        if (newI.contains("class java.lang.")) {
+            print(newI.replace("class java.lang.", "") + "\n")
+        }
+        if (newI.contains("class java.util.")){
+            print(newI.replace("class java.util.", "") + "\n")
+        }
+    }
 }
--- src/mnozhestva/3.kt
+++ src/mnozhestva/3.kt
@@ -1,5 +1,20 @@
 package mnozhestva
 
-fun main() {
+// Найти Элементы множества, не встречающиеся во 2 множестве
 
+fun main() {
+    val set1 = mutableSetOf<Int>()
+    val set2 = mutableSetOf<Int>()
+
+    val num1 = readLine()!!.toInt()
+    val num2 = readLine()!!.toInt()
+
+    for (item: Int in 0..num1-1) {
+        set1.add(readLine()!!.toInt())
+    }
+    for (item: Int in 0..num2-1) {
+        set2.add(readLine()!!.toInt())
+    }
+
+    print(set1 - set1.intersect(set2))
 }
