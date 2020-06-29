6ef63f2c7a91faffde1c81c77093a4d87282e91b
VladosDudos1 <55552724+VladosDudos1@users.noreply.github.com>
2020-06-29 14:06:29 +0300

    last

--- src/slovar/2.kt
+++ src/slovar/2.kt
@@ -1,5 +1,54 @@
 package slovar
 
-fun main() {
+import kotlin.math.max
 
+//Найдите в словаре самый длинный элемент (не уточняется, найти )
+
+fun main() {
+    val listK = arrayListOf<String>()
+    val listI = arrayListOf<String>()
+    val num1 = readLine()!!.toInt()
+    val num2 = readLine()!!.toInt()
+
+    val dictionary = mutableMapOf<String, String>()
+
+
+    for (item: Int in 0..num1 - 1) {
+        listK.add(readLine().toString())
+    }
+
+    for (item: Int in 0..num2 - 1) {
+        listI.add(readLine().toString())
+    }
+    if (listK.size > listI.size) {
+        for (i in 0..listK.size - 1) {
+            if (listI.lastIndex < i) {
+                dictionary.put(listK[i], "None")
+            } else dictionary.put(listK[i], listI[i])
+        }
+    }
+
+    if (listK.size < listI.size) {
+        for (i in 0..listK.size - 1) {
+            if (listK.lastIndex >= i) {
+                dictionary.put(listK[i], listI[i])
+            }
+        }
+    }
+    if (listK.size == listI.size) {
+        for (i in 0..listK.size - 1) {
+            dictionary.put(listK[i], listI[i])
+        }
+    }
+
+    val newList = mutableListOf<Int>()
+    val checkList = mutableListOf<String>()
+
+    for (i in dictionary){
+        newList.add(i.value.length)
+        checkList.add(i.value)
+    }
+
+    print(newList.max().toString() + "\n")
+    print(checkList[newList.indexOf(newList.max())])
 }
